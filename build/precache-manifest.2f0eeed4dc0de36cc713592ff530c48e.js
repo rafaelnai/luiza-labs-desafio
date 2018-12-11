5b006e14627af94543d6393a56a7a65491f47f57
Rafael Nai <ramiresnai@gmail.com>
2018-12-11 10:59:26 -0200

    production deploy

---
+++ build/precache-manifest.2f0eeed4dc0de36cc713592ff530c48e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65913442fa0e01380747",
+    "url": "/static/js/main.65913442.chunk.js"
+  },
+  {
+    "revision": "d07a6aae2a0f84ea31ef",
+    "url": "/static/js/1.d07a6aae.chunk.js"
+  },
+  {
+    "revision": "65913442fa0e01380747",
+    "url": "/static/css/main.45f36171.chunk.css"
+  },
+  {
+    "revision": "b1721722fbd9c1aa41dc60c9711d4fbd",
+    "url": "/index.html"
+  }
+];
