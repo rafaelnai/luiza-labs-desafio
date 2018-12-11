fb1d14e730bdc2aa5191e92d0c3e824e76e1645a
Rafael Nai <ramiresnai@gmail.com>
2018-12-11 08:54:17 -0200

    added pattern on input

--- src/components/AddressQuery/AddressQuery.js
+++ src/components/AddressQuery/AddressQuery.js
@@ -20,7 +20,7 @@ const AddressQuery = props => {
 
         <label className="address-query__form--label" htmlFor="cep">CEP</label>
         <input
-          type="tel"
+          pattern="[0-9]*"
           inputMode="numeric"
           className="address-query__form--field"
           onChange={e => handleChange(e)}
